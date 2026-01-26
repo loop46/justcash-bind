2e0647e2262387202b4ac37d4816698f6ba8c46b
anarchodev <an@rcho.dev>
2026-01-26 16:43:26 +0000

    bump version

--- jclib/build.gradle.kts
+++ jclib/build.gradle.kts
@@ -9,7 +9,7 @@ publishing {
         register<MavenPublication>("gpr") {
             groupId = "com.loop46"
             artifactId = "justcash"
-            version = "0.0.1"
+            version = "0.0.2"
 
             afterEvaluate {
                 from(components["release"])
